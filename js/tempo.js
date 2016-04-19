cf79afeebce82a08927883e0c23677654a85208a
ac22 <anandc@fastmail.com>
2016-04-18 20:59:05 -0400

    Add h1 to display the estimated bpm

--- js/tempo.js
+++ js/tempo.js
@@ -1,30 +1,46 @@
-var beatHistory = []; // stores the times the drum was kicked betwen beats
-var canvas = document.querySelector('#canvas');
-const ESC = 27; //press ESC to reset the bpm
+var beatHistory = [];       // stores the times the drum was kicked betwen beats
+var bpm = 0;                // stores beats per minute estimate
+var BPM_INDICATOR;
+var ESC = 27;             //press ESC to reset the bpm
+var USE_AT_MOST = 9;      // the max number of past beats to consider for bpm calculation
 
 function init() {
-    window.addEventListener('keydown', keyEventHandler, false);
-    window.addEventListener('mousedown', kickDrum, false);
+    window.addEventListener('keydown', keyEventHandler);
+    window.addEventListener('mousedown', addBeat);
+    BPM_INDICATOR = document.querySelector('#bpm');
 }
 
 function keyEventHandler(e) {
     if (e.keyCode === ESC) {
         beatHistory = [];
         console.log('Cleared beats');
+        BPM_INDICATOR.innerHTML = 'Tap Tap Tap';
     } else {
-        kickDrum();
-        bpm = calcBPM(beatHistory);
-        console.log(bpm);
+        addBeat();
+    }
+}
+
+function addBeat() {
+    kickDrum();
+    beatHistory.push(Date.now());
+    bpm = calcBPM(beatHistory, USE_AT_MOST);
+    if (bpm) {
+        bpm = Math.round(bpm);
+        BPM_INDICATOR.innerHTML = bpm + ' bpm';
     }
 }
 
 function kickDrum() {
-    beatHistory.push(Date.now());
     // TODO: playSoundEffect();
     // TODO: doAnimation();
 }
 
-function calcBPM(beatHistory, useAtMost=3) {
+/**
+ * Calculates beats per minute
+ * The calculation will use only useAtMost of the last inputs for the calculation
+ * Figure out the total time elapsed, divide by beats, convert milliseconds to minutes
+ */
+function calcBPM(beatHistory, useAtMost) {
     if (!beatHistory || beatHistory.length < 2) {
         return undefined;
     } else {
