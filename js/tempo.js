dede85105794f330773f7eb491504bd026080d3e
Anand Chandran <anandc@fastmail.com>
2016-04-23 01:37:29 -0400

    Clean up code to follow airbnb javascript style

--- js/tempo.js
+++ js/tempo.js
@@ -1,52 +1,45 @@
-var beatHistory = [];       // stores the times the drum was kicked betwen beats
-var bpm = 0;                // stores beats per minute estimate
-var BPM_INDICATOR;
-var ESC = 27;               //press ESC to reset the bpm
-var USE_AT_MOST = 2;        // the max number of past beats to consider for bpm calculation
+let beatHistory = [];       // stores the times the drum was kicked betwen beats
+let bpm = 0;                // stores beats per minute estimate
+let BPM_INDICATOR;
+const ESC = 27;             // press ESC to reset the bpm
+const USE_AT_MOST = 2;      // the max number of past beats to consider for bpm calculation
 
-function init() {
-    window.addEventListener('keydown', keyEventHandler);
-    window.addEventListener('mousedown', addBeat);
-    BPM_INDICATOR = document.querySelector('#bpm');
-}
-
-function keyEventHandler(e) {
-    if (e.keyCode === ESC) {
-        beatHistory = [];
-        console.log('Cleared beats');
-        BPM_INDICATOR.innerHTML = 'Tap Tap Tap';
-    } else {
-        addBeat();
-    }
-}
-
-function addBeat() {
-    kickDrum();
-    beatHistory.push(Date.now());
-    bpm = calcBPM(beatHistory, USE_AT_MOST);
-    if (bpm) {
-        bpm = Math.round(bpm);
-        BPM_INDICATOR.innerHTML = bpm + ' bpm';
-    }
-}
-
-function kickDrum() {
-    // TODO: playSoundEffect();
-    // TODO: doAnimation();
-}
 
 /**
  * Calculates beats per minute
- * The calculation will use only useAtMost of the last inputs for the calculation
+ * The calculation will use only USE_AT_MOST of the last inputs for the calculation
  * Figure out the total time elapsed, divide by beats, convert milliseconds to minutes
  */
-function calcBPM(beatHistory, useAtMost) {
-    if (!beatHistory || beatHistory.length < 2) {
-        return undefined;
-    } else {
-        var len = beatHistory.length;
-        var use = Math.min(useAtMost, len - 1);
-        var timePerBeat = (beatHistory[len - 1] - beatHistory[len - 1 - use]) / use;
-        return (1 / timePerBeat) * 1000 * 60;
-    }
+function calcBPM() {
+  if (!beatHistory || beatHistory.length < 2) {
+    return undefined;
+  }
+  const len = beatHistory.length;
+  const use = Math.min(USE_AT_MOST, len - 1);
+  const timePerBeat = (beatHistory[len - 1] - beatHistory[len - 1 - use]) / use;
+  return (1 / timePerBeat) * 1000 * 60;
 }
+
+function addBeat() {
+  beatHistory.push(Date.now());
+  bpm = calcBPM(beatHistory, USE_AT_MOST);
+  if (bpm) {
+    bpm = Math.round(bpm);
+    BPM_INDICATOR.innerHTML = `${bpm} bpm`;
+  }
+}
+
+function keyEventHandler(e) {
+  if (e.keyCode === ESC) {
+    beatHistory = [];
+    BPM_INDICATOR.innerHTML = 'Tap Tap Tap';
+  } else {
+    addBeat();
+  }
+}
+
+(function init() {
+  window.addEventListener('keydown', keyEventHandler);
+  window.addEventListener('mousedown', addBeat);
+  BPM_INDICATOR = document.querySelector('#bpm');
+}());
